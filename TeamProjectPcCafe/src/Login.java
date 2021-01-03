78bab29365e8297d9f98af13dd8e1799672a7e4e
wkmoo <wkmoo@DESKTOP-1KR9V80>
2021-01-04 07:15:03 +0900

    Make more Class

---
+++ TeamProjectPcCafe/src/Login.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class Login {
+
+	public static void main(String[] args) {
+		JFrame f = new JFrame("로그인");
+		JLabel l1 = new JLabel("사용자:");
+		l1.setBounds(20,20,80,30);
+		JTextField text = new JTextField();
+
+	}
+
+}
---
+++ TeamProjectPcCafe/src/Total.java
@@ -0,0 +1,9 @@
+
+public class Total {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
