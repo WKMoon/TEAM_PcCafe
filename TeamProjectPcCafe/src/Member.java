3ec429881361acc582913174fb55cf5ff150f4c6
wkmoo <wkmoo@DESKTOP-1KR9V80>
2021-01-03 20:42:52 +0900

    Processing

--- TeamProjectPcCafe/src/Member.java
+++ TeamProjectPcCafe/src/Member.java
@@ -1,13 +1,24 @@
 
 public class Member {
-
+	//회원가입
 	String name;
 	String password;
-	String addr;
+	int age;
 	String phone;
+	
+	//로그인
 	int pcNum;
 
-	Member(String name, String addr, String phone){}
-	Member(String name, String password, int pcNum){}
+	Member(String name, String password, int age, String phone){
+		this.name = name;
+		this.password = password;
+		this.age = age;
+		this.phone = phone;
+	}
+	Member(String name, String password, int pcNum){
+		this.name = name;
+		this.password = password;
+		this.pcNum = pcNum;
+	}
 	
 }//end Member
