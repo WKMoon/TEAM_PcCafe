ca2faede3d9ee0101afd44b67856b2bf1ea68727
wkmoo <wkmoo@DESKTOP-1KR9V80>
2021-01-04 06:27:57 +0900

    processing

--- TeamProjectPcCafe/src/Member.java
+++ TeamProjectPcCafe/src/Member.java
@@ -15,6 +15,36 @@ public class Member {
 		this.age = age;
 		this.phone = phone;
 	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public int getPcNum() {
+		return pcNum;
+	}
+	public void setPcNum(int pcNum) {
+		this.pcNum = pcNum;
+	}
 	Member(String name, String password, int pcNum){
 		this.name = name;
 		this.password = password;
